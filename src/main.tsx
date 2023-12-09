f0a1c8cb5dbacce1eed944a9568f7429afe2c396
Yan <yan.simrok@gmail.com>
2023-12-10 00:57:44 +0300

    up

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { configureStore } from '@reduxjs/toolkit' //REdux toolkit!
+// import { configureStore } from '@reduxjs/toolkit' //REdux toolkit!
 //Thunk
 //useReducer в React
 
-interface State {
-  totalPage: number;
-}
+// interface State {
+//   totalPage: number;
+// }
 
-type Action = { type: "SET_TOTAL_PAGE"; payload: {} } | { type: string };
+// type Action = { type: "SET_TOTAL_PAGE"; payload: {} } | { type: string };
 
-const reducer = (state: State, action: Action): State => {
-  switch (action.type) {
-    case "SET_TOTAL_PAGE":
-      return { ...state, totalPage: state.totalPage + action.payload };
-    default:
-      return state;
-  }
-};
+// const reducer = (state: State, action: Action): State => {
+//   switch (action.type) {
+//     case "SET_TOTAL_PAGE":
+//       return { ...state, totalPage: state.totalPage + action.payload };
+//     default:
+//       return state;
+//   }
+// };
 
 
-const store = configureStore(reducer)
+// const store = configureStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
